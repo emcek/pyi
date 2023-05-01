cae484ed4cf8efdc32170783eba2d827a59e3b6d
Michal Plichta <mplichta@gmail.com>
2023-05-01 21:36:53 +0200

    update 1.8.34

--- latest_tag.py
+++ latest_tag.py
@@ -5,12 +5,12 @@ from packaging import version
 import git
 
 
-def latest_tag(repo_path: str, as_ver=False) -> str:
+def latest_tag(repo_path: str, as_tag=1) -> str:
     repo = git.Repo(repo_path)
     tags_list = [str(tag) for tag in repo.tags]
     ver_tags = [version.parse(m.group(1)) for tag in tags_list if (m := match(r'v(\d+\.\d+\.\d+)$', tag))]
     tag = str(max(ver_tags))
-    if as_ver:
+    if int(as_tag):
         tag = f'v{tag}'
     return tag
 
