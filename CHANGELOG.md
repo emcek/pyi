05403182c7467bf42503ae17553358291bb16778
Michal Plichta <mplichta@gmail.com>
2023-05-05 20:23:02 +0200

    update 1.8.72

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 1.8.71
+## 1.8.72
 * Allow use/update DCS-BIOS directly from GitHub (master branch)
 * Allow run DCSpy without console
 * Auto screenshot of LCD during operation
--- latest_tag.py
+++ latest_tag.py
@@ -6,6 +6,13 @@ import git
 
 
 def latest_version(repo_path: str, as_tag=1) -> str:
+    """
+    Get latest version number form repository.
+
+    :param repo_path: path to repository
+    :param as_tag: if True return full tag name
+    :return: return version or tag name as string
+    """
     repo = git.Repo(repo_path)
     tags_list = [str(tag) for tag in repo.tags]
     ver_tags = [version.parse(m.group(1)) for tag in tags_list if (m := match(r'v(\d+\.\d+\.\d+)$', tag))]
