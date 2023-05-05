ca16e247ce8952003d5b7a152e3ae206eb129530
Michal Plichta <mplichta@gmail.com>
2023-05-05 22:17:15 +0200

    update 1.8.77

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 1.8.76
+## 1.8.77
 * Allow use/update DCS-BIOS directly from GitHub (master branch)
 * Allow run DCSpy without console
 * Auto screenshot of LCD during operation
--- dcspy/run.py
+++ dcspy/run.py
@@ -10,7 +10,7 @@ from dcspy.tk_gui import DcspyGui
 from dcspy.utils import check_dcs_ver
 
 LOG = getLogger(__name__)
-__version__ = '1.8.76'
+__version__ = '1.8.77'
 
 
 def run():
