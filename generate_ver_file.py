1c984e6a6a17b3955d1bdaaf8cde3a4eb13cb367
Michal Plichta <mplichta@gmail.com>
2024-09-09 17:26:53 +0200

    Update version file generation to use decimal git SHA

--- generate_ver_file.py
+++ generate_ver_file.py
@@ -5,7 +5,7 @@ from sys import argv
 from PyInstaller.utils.win32 import versioninfo
 
 
-def generate_ver_info(major: int, minor: int, patch: int, build: int, git_sha: str) -> versioninfo.VSVersionInfo:
+def generate_ver_info(major: int, minor: int, patch: int, build: int, dec_git_sha: int) -> versioninfo.VSVersionInfo:
     """
     Generate a version information object.
 
@@ -13,7 +13,7 @@ def generate_ver_info(major: int, minor: int, patch: int, build: int, git_sha: s
     :param minor: Version minor part
     :param patch: Version patch part
     :param build: GitHub build number
-    :param git_sha: Git SHA hash
+    :param dec_git_sha: Decimal representation of 4 first character of Git SHA hash
     :return: VSVersionInfo object
     """
     ver_info = versioninfo.VSVersionInfo(
@@ -34,7 +34,7 @@ def generate_ver_info(major: int, minor: int, patch: int, build: int, git_sha: s
               versioninfo.StringStruct('LegalCopyright', '© 2023 Michał Plichta. All rights reserved.'),
               versioninfo.StringStruct('OriginalFilename', 'dcs_py.exe'),
               versioninfo.StringStruct('ProductName', 'DCSpy'),
-              versioninfo.StringStruct('ProductVersion', f'{major}.{minor}.{patch} ({git_sha})')])]),
+              versioninfo.StringStruct('ProductVersion', f'{major}.{minor}.{patch}.{dec_git_sha}')])]),
               versioninfo.VarFileInfo([versioninfo.VarStruct('Translation', [1033, 1200])])])
     return ver_info
 
@@ -52,15 +52,16 @@ def save_ver_file(ver=environ.get('GITHUB_REF_NAME', '3.6.8'), bld=environ.get('
     :param ver_f: File name of version file
     :return: Input parameters
     """
+    dec_git_sha = int(sha[:4], 16)
     if all([ver, bld, sha, ver_f]):
         if ver.startswith('v'):
             ver = ver[1:]
-        info_ver_data = generate_ver_info(*[int(i) for i in ver.split('.')], build=int(bld), git_sha=sha[:7])
+        info_ver_data = generate_ver_info(*[int(i) for i in ver.split('.')], build=int(bld), dec_git_sha=dec_git_sha)
         with open(ver_f, mode='w+', encoding='utf-8') as file_ver_info:
             file_ver_info.write(str(info_ver_data))
     else:
         print('Use: v1.9.5 40 a43c77649cad77204c075b431f38d03dbc75cbdd file_version_info.txt')
-    return ver, bld, sha[:7], ver_f
+    return ver, bld, str(dec_git_sha), ver_f
 
 
 if __name__ == '__main__':
