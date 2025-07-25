84af6fa29e436523dea91fd7061148e000aaf97f
Michal Plichta <mplichta@gmail.com>
2025-07-25 17:07:07 +0200

    3.11.20

--- src/dcs_py.py
+++ src/dcs_py.py
@@ -5,7 +5,7 @@ from traceback import format_exc
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication, QMessageBox
 
-__version__ = '3.11.19'
+__version__ = '3.11.20'
 
 
 def start_dcspy(cli_args: Namespace) -> None:
