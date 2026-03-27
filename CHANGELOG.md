092a3d25e9ad5022f1fe9a569efe5639a5c8cb36
Michal Plichta <mplichta@gmail.com>
2026-03-27 16:16:21 +0100

    3.11.57

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 3.11.56
+## 3.11.57
 * Fix debugger tab in dark mode - #551 (@emcek)
 * Switching color schem, switching images and logos in Application - #554 (@emcek)
 * Internal:
--- src/dcs_py.py
+++ src/dcs_py.py
@@ -5,7 +5,7 @@ from traceback import format_exc
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication, QMessageBox
 
-__version__ = '3.11.56'
+__version__ = '3.11.57'
 
 
 def start_dcspy(cli_args: Namespace) -> None:
